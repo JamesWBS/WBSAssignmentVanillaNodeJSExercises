38bc9bfbe4f99df1d5e7e97f334d84b73046c9df
Your Name <you@example.com>
2020-05-10 00:00:03 +0200

    Initial Commit
    Initial Commit Exercise 1 completed

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+// ANCHOR EXERCISE 1 INSTRUCTIONS //
+// Create a code that displays `hello` in the console.
+// To launch this file, type in your terminal `node exercise1.js`.
+
+// SECTION DISPLAY HELLO
+
+console.log("Hello, Exercise 1!"); // send Content body "Hello, Exercise 1!" to terminal
